841107744a2dfc9949f1743de5826610c9fa822f
Zubair Bhatti <djzubah@gmail.com>
2020-02-01 12:54:12 -0500

    make seperate train and generate scripts

--- examples/ttbar/madminer_generate.sh
+++ examples/ttbar/madminer_generate.sh
@@ -5,19 +5,9 @@
 #SBATCH --nodes=1
 
 ## use for generate:
-##SBATCH --mem=16GB
-##SBATCH --time=2-23:00:00
-##SBATCH --cpus-per-task=2
-
-## use for train:
-#SBATCH --mem=150GB
-#SBATCH --time=20:00:00
-#SBATCH --cpus-per-task=4
-
-# unused:
-##SBATCH --gres=gpu:1
-##SBATCH --mail-user=zb609@nyu.edu
-##SBATCH --mail-type=END
+#SBATCH --mem=16GB
+#SBATCH --time=2-23:00:00
+#SBATCH --cpus-per-task=2
 
 export PATH=$PATH:$HOME/anaconda2/bin
 source $HOME/anaconda2/etc/profile.d/conda.sh
@@ -32,5 +22,4 @@ export MADGRAPH_DIR=/home/zb609/scratch_dir/MG5_aMC_v2_6_5
 # RUN SETUP AS YOURSELF
 # python batch_madminer_event_runner.py ~/scratch_dir/madminer_data_6_dup setup
 
-#python -u batch_madminer_event_runner.py $1 generate $2 ${SLURM_ARRAY_TASK_ID}
-python -u batch_madminer_event_runner.py $1 train
+python -u batch_madminer_event_runner.py $1 generate $2 ${SLURM_ARRAY_TASK_ID}
---
+++ examples/ttbar/madminer_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=zbhatti_madminer
+#SBATCH --output=log_%a_%j.log
+#SBATCH --nodes=1
+
+## use for train:
+#SBATCH --mem=150GB
+#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=4
+
+export PATH=$PATH:$HOME/anaconda2/bin
+source $HOME/anaconda2/etc/profile.d/conda.sh
+
+conda activate python-mm-27
+conda env list
+source /scratch/zb609/root_build/bin/thisroot.sh
+
+cd $SCRATCH/madminer/examples/ttbar
+export MADGRAPH_DIR=/home/zb609/scratch_dir/MG5_aMC_v2_6_5
+
+python -u batch_madminer_event_runner.py $1 train
